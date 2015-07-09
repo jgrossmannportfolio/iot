50df538d3b3e091aa2f66fa77667286ceecf1ff0
John Grossmann <jgrossmann@colgate.edu>
2015-07-08 22:34:46 -0400

    animation template

--- UADGolf/cloudCode/public/javascripts/appclient.js
+++ UADGolf/cloudCode/public/javascripts/appclient.js
@@ -1,4 +1,8 @@
 var myapp = (function(){
+    var puttData = [];
+    var width = 600;
+    var height = 400;
+    var animated = false;
 
     var removeUsers = function() {
         console.log("inside remove users");
@@ -15,10 +19,122 @@ var myapp = (function(){
         });
     }
 
+    function setCanvasDim() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        $("#puttCanvas").attr("width", width);
+        $("#puttCanvas").attr("height", height / 2.0);
+        console.log("width: "+$("#puttCanvas").attr("width"));
+        console.log("set canvas dimensions");
+    };
+
+    function getPuttData() {
+        setCanvasDim();
+        
+        console.log("getting put data");
+        var putt = Parse.Object.extend("Putt");
+        var query = new Parse.Query(putt);
+        query.get("12fz4AHTDK", {
+            success: function(data) {
+                puttData = data.get("frames");   
+                console.log("got data");
+                console.log(puttData);   
+            },
+            error: function(object, error) {
+                console.log("Error getting putt data");
+                console.log(error);
+            }
+        });
+    };
+
+    function animatePutt() {
+        if(animated) {
+            return;
+        }else {
+            window.requestAnimFrame = (function(callback) {
+                return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+                function(callback) {
+                  window.setTimeout(callback, 1000 / 60);
+                };
+            })();
+        }
+
+      function drawRectangle(myRectangle, context) {
+        context.beginPath();
+        context.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
+        context.fillStyle = '#8ED6FF';
+        context.fill();
+        context.lineWidth = myRectangle.borderWidth;
+        context.strokeStyle = 'black';
+        context.stroke();
+      }
+      function animate(myRectangle, canvas, context, frame, scale) {
+        // update
+        //var time = (new Date()).getTime() - startTime;
+
+        //var linearSpeed = 100;
+        // pixels / second
+        //var newX = linearSpeed * time / 1000;
+        console.log("frame: "+frame);
+        var newX = puttData[frame][0] / scale;
+        var newY = puttData[frame][1] / scale;
+        var newZ = puttData[frame][2] / scale;
+
+        /*if(newX < canvas.width - myRectangle.width - myRectangle.borderWidth / 2) {
+          myRectangle.x = newX;
+        }*/
+        
+        myRectangle.x = myRectangle.x + newX;
+       // myRectangle.y = myRectangle.y + newY;
+
+        // clear
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        drawRectangle(myRectangle, context);
+
+        // request new frame
+        requestAnimFrame(function() {
+          if(frame+1 < puttData.length) {
+            animate(myRectangle, canvas, context, frame+1, scale);
+          }else {
+            animated = false;
+            console.log("animation completed");
+            return;
+          }
+        });
+      }
+      var canvas = document.getElementById('puttCanvas');
+      var context = canvas.getContext('2d');
+
+      var myRectangle = {
+        x: (width / 2.0) - 25,
+        y: (height / 4.0) - 50,
+        width: 50,
+        height: 100,
+        borderWidth: 5
+      };
+
+      drawRectangle(myRectangle, context);
+
+      // wait one second before starting animation
+      setTimeout(function() {    
+        console.log(puttData);
+        if(puttData != [] && puttData != null) {   
+            animated = true;    
+            animate(myRectangle, canvas, context, 0, 10.0);
+        }else {
+            console.log("no putt data loaded");
+        }
+      }, 1000);
+    };
    
 
     return {
         init: function() {
+            console.log("starting up");
+            Parse.initialize("iAFEw9XwderX692l0DGIwoDDHcLTGMGtcBFbgMqb", "d4q6T1YxleQ2za17PdTtfaAD8x6tAB9rdW4y9vMD");
+            $(window).load(getPuttData);
+            jQuery("#startButton").click(animatePutt);
             //console.log("Client-side app starting up")
             
 			//jQuery("#testbutton").click(test);
@@ -27,4 +143,6 @@ var myapp = (function(){
     }
 })();
 jQuery(myapp.init);
+
+
 
