1cf410e8186041792e33f52fd0b03c438089a837
Peter <peter.burrows@columbia.edu>
2015-07-27 12:20:37 -0400

    Asd

--- test/accelFilterTest.py
+++ test/accelFilterTest.py
@@ -19,14 +19,26 @@ new = re.split(', |\[|\, |\,|\]', data)
 
 
 
-def create3plots(yMin,yMax,yticks,plotData):
+def create3plots(yticks,plotData):
     # plotData must be in the form of [xdataArray, ydataArray, zdataArray]
-    xmax = timestamp[-1]
-    xmin = 0
-    axesRange = [xmin, xmax, yMin, yMax]
-
 
     # first sub plotting
+    # - configure the axes sizes
+    ymin0 = min(plotData[0])
+    ymin1 = min(plotData[1])
+    ymin2 = min(plotData[2])
+
+    ymax0 = max(plotData[0])
+    ymax1 = max(plotData[1])
+    ymax2 = max(plotData[2])
+
+    ymax = max([ymax0,ymax1,ymax2])
+    ymin = min([ymin0,ymin1,ymin2])
+    xmax = timestamp[-1]
+    xmin = 0
+    axesRange = [xmin, xmax, ymin, ymax]
+    
+    yTicks = (ymax - ymin)/yticks 
     fig = plt.figure()
     
     ax = fig.add_subplot(131)
@@ -36,44 +48,50 @@ def create3plots(yMin,yMax,yticks,plotData):
     ax.set_xlabel(r'Time ($s$)')
 
     ax.set_xticks(np.arange(xmin,xmax,delta_t))
-    ax.set_yticks(np.arange(ymin,ymax,yticks))
-
+    ax.set_yticks(np.arange(ymin,ymax,yTicks))
 
     ax.plot(timestamp, plotData[0], 'o')
 
     ax.axis(axesRange)
-    plt.grid()
-
+    ax.grid(True)
+    #ax2.set_xticklabels([])
 
     # second subplot
+    # - configure the axes sizes
+
     ax2 = fig.add_subplot(132)
     fig.subplots_adjust(top=0.85)
     ax2.set_title(r'$Y$')
-
+    
     ax2.set_xlabel(r'Time ($s$)')
 
     ax2.set_xticks(np.arange(xmin,xmax,delta_t))
-    ax2.set_yticks(np.arange(ymin,ymax,yticks))
+    ax2.set_yticks(np.arange(ymin,ymax,yTicks))
 
     ax2.plot(timestamp, plotData[1], 'o', color='red')
 
     ax2.axis(axesRange)
-    plt.grid()
+    #plt.grid()
+    ax2.grid(True)
+    ax2.set_yticklabels([])
 
-    # second subplot
+    # third subplot
+    # - configure the axes sizes
+
     ax3 = fig.add_subplot(133)
     fig.subplots_adjust(top=0.85)
-    ax3.set_title(r'$Y$')
+    ax3.set_title(r'$Z$')
 
     ax3.set_xlabel(r'Time ($s$)')
 
     ax3.set_xticks(np.arange(xmin,xmax,delta_t))
-    ax3.set_yticks(np.arange(ymin,ymax,yticks))
+    ax3.set_yticks(np.arange(ymin,ymax,yTicks))
 
     ax3.plot(timestamp, plotData[2], 'o', color='green')
 
     ax3.axis(axesRange)
-    plt.grid()
+    ax3.grid(True)
+    ax3.set_yticklabels([])
 
     return ax, fig
 
@@ -128,12 +146,10 @@ dzData, vzData = getVeloAndDispLists(azData)
 
 # Create Accel xyz plots
 #   -set axes
-ymin = -1
-ymax = 1
-yTicks = .1 # spacing per ticks
+yTicks = 10 # number of ticks
 accelPlotData = [axData, ayData, azData]
 
-ayHandle, figAccel = create3plots(ymin,ymax,yTicks,accelPlotData)
+ayHandle, figAccel = create3plots(yTicks,accelPlotData)
 figAccel.suptitle('Acceleration Data', fontsize=14, fontweight='bold')
 ayHandle.set_ylabel(r'Acceleration ($m/s^2$)')
 
@@ -141,7 +157,7 @@ ayHandle.set_ylabel(r'Acceleration ($m/s^2$)')
 # Create Velo xyz plots
 veloPlotData = [vxData, vyData, vzData]
 
-vyHandle, figVelo = create3plots(ymin,ymax,yTicks,veloPlotData)
+vyHandle, figVelo = create3plots(yTicks,veloPlotData)
 figVelo.suptitle('Velocity Calculation', fontsize=14, fontweight='bold')
 vyHandle.set_ylabel(r'Velocity ($m/s$)')
 
@@ -149,9 +165,10 @@ vyHandle.set_ylabel(r'Velocity ($m/s$)')
 # Create Displacement xyz plots
 dispPlotData = [dxData, dyData, dzData]
 
-dyHandle, figDisp = create3plots(ymin,ymax,yTicks,dispPlotData)
+dyHandle, figDisp = create3plots(yTicks,dispPlotData)
 figDisp.suptitle('Displacement Calculation', fontsize=14, fontweight='bold')
 dyHandle.set_ylabel(r'Displacement ($m$)')
+
 
 
 plt.show()
